5cc1668951ac50feb31e7ebd9cc8e5c059104d44
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-14 00:13:01 -0800

    Added employees to employees database.

--- employees/src/components/employeesList/index.js
+++ employees/src/components/employeesList/index.js
@@ -6,24 +6,26 @@ function List(props) {
   <table className="table table-dark">
   <thead>
     <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
+      <th scope="col">Profile</th>
+      <th scope="col">Name</th>
+      <th scope="col">Phone</th>
+      <th scope="col">Email</th>
+      <th scope="col">Date of Birth</th>
     </tr>
   </thead>
   <tbody>
-  {props.data.map((card) => (
-          // <div key={card.id} className="card" onClick={props.handleClick}>
+  {/* {props.data.map((card) => ( */}
+          {/* // <div key={card.id} className="card" onClick={props.handleClick}>
           //   <img id={card.id} src={card.image} className="card-img-top" />
-          // </div>
+          // </div> */}
           <tr>
           <th scope="row">1</th>
           <td>Mark</td>
           <td>Otto</td>
           <td>@mdo</td>
+          <td>3-28-2001</td>
         </tr>
-        ))}
+        {/* ))} */}
   </tbody>
 </table>
 </article>;
