2b70cc07d3bf2e8961a8ff621fc929a12af2becd
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-20 18:16:08 -0800

    Fixed toggle and filtering sort

--- employees/src/App.js
+++ employees/src/App.js
@@ -1,45 +1,57 @@
-import React, {useReducer, useState} from "react";
+import React, { useEffect, useReducer, useRef, useState } from "react";
 import Wrapper from "./components/wrapper";
 import Banner from "./components/banner";
-import List from "./components/employeesList"
-import Employees from "./components/employees.json"
-import './App.css';
+import List from "./components/employeesList";
+import employeesArray from "./components/employees.json";
+import "./App.css";
 
-function reducer(state, action) {
-  switch (action.type) {
-    case "sort":
-      return [...state].sort((a, b)=>(a.name > b.name) ? 1: -1);
-    default:
-      return state;
-    }
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case "sort":
+//       return [...state].sort((a, b) => (a.name > b.name ? 1 : -1));
+//     default:
+//       return state;
+//   }
+// }
+// function handleToggle(toggle, action){
+//   console.log("click")
 
-}
-function handleToggle(state, action){
-  console.log("click")
-  
-    if (state === true){
-      // setEmployees({ type: "sort" })
-      console.log(state);
-    }
-}
-
+// }
 
 function App() {
-const [employees, setEmployees] = useReducer(reducer, Employees);
-const [text, setText] = useState("");
+  const [employees, setEmployees] = useState(employeesArray);
+  const [text, setText] = useState("");
 
-  const [toggle, setToggle] = useState(handleToggle, false);
-  
-
-
-
+  const [toggle, setToggle] = useState(false);
+  useEffect(function () {
+    if (toggle === "ascending") {
+      console.log(true)
+      setEmployees([...employees].sort((a, b) => (a.name > b.name ? 1 : -1)));
+      console.log(toggle);
+    } else if (toggle === "descending") {
+      console.log(false)
+      setEmployees([...employees].sort((a, b) => (a.name > b.name ? -1 : 1)));
+    } else {
+      setEmployees(employeesArray);
+    }
+  }, [toggle]);
 
   return (
     <div className="App">
       <Wrapper>
         <Banner />
-        <input className="input col-sm-6" value={text} onChange={e => setText(e.target.value)} placeholder="Search Employees"/>
-        <List employees={employees} toggle={toggle} setToggle={setToggle} text={text}/>
+        <input
+          className="input col-sm-6"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Search Employees"
+        />
+        <List
+          employees={employees}
+          toggle={toggle}
+          setToggle={setToggle}
+          text={text}
+        />
       </Wrapper>
     </div>
   );
--- employees/src/components/employeesList/index.js
+++ employees/src/components/employeesList/index.js
@@ -1,34 +1,60 @@
-import React, {useReducer} from "react";
+import React from "react";
 import "./style.css";
 
-function List( {employees, toggle, setToggle, handleToggle, text}) {
-  return <article className="container d-flex justify-content-center">
-  <table className="table table-dark">
-  <thead>
-    <tr>
-      <th scope="col">Profile</th>
-      <th scope="col"><a onClick ={handleToggle, () => setToggle(!toggle)}>Name</a>
-      </th>
-      <th scope="col">Phone</th>
-      <th scope="col">Email</th>
-      <th scope="col">Date of Birth</th>
-    </tr>
-  </thead>
-  <tbody>
-  {employees.filter(employee => employee.name.includes(text)).map(filteredEmployee => (
+function List({ employees, toggle, setToggle, text }) {
+  return (
+    <article className="container d-flex justify-content-center">
+      <table className="table table-dark">
+        <thead>
           <tr>
-          <th key={filteredEmployee.id} scope="row">
-          <img src={filteredEmployee.image} alt={filteredEmployee.name} />
-          </th>
-          <td className={filteredEmployee.name}>{filteredEmployee.name}</td>
-          <td>{filteredEmployee.phone}</td>
-          <td>{filteredEmployee.email}</td>
-          <td>{filteredEmployee.dob}</td>
-        </tr>
-        ))}
-  </tbody>
-</table>
-</article>;
+            <th scope="col">Profile</th>
+            <th scope="col">
+              <a
+                onClick={() =>
+                  { 
+                    if (toggle === "ascending"){
+                      return setToggle("descending");
+                    }
+                    if (toggle === "descending"){
+                      return setToggle(false);
+                    }
+                    if (!toggle){
+                      return setToggle("ascending");
+                    }
+                  }
+                }
+              >
+                Name
+              </a>
+            </th>
+            <th scope="col">Phone</th>
+            <th scope="col">Email</th>
+            <th scope="col">Date of Birth</th>
+          </tr>
+        </thead>
+        <tbody>
+          {employees
+            .filter((employee) => employee.name.includes(text))
+            .map((filteredEmployee) => (
+              <tr key={filteredEmployee.id}>
+                <th scope="row">
+                  <img
+                    src={filteredEmployee.image}
+                    alt={filteredEmployee.name}
+                  />
+                </th>
+                <td className={filteredEmployee.name}>
+                  {filteredEmployee.name}
+                </td>
+                <td>{filteredEmployee.phone}</td>
+                <td>{filteredEmployee.email}</td>
+                <td>{filteredEmployee.dob}</td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </article>
+  );
 }
 
 export default List;
